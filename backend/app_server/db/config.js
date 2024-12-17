fddb0298409d5999591271b85bd4dae116391450
Daniel Serrano <51222713+DanielDev200@users.noreply.github.com>
2024-12-16 18:06:12 -0800

    debugging

--- backend/app_server/db/config.js
+++ backend/app_server/db/config.js
@@ -12,5 +12,12 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+console.log('DB Config:', {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
 // Export the pool directly
 module.exports = pool;
