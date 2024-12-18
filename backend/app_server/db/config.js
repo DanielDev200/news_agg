5f5ad84a02112f83f3286247ffda76a45c3d35f6
Daniel Serrano <51222713+DanielDev200@users.noreply.github.com>
2024-12-18 08:21:07 -0800

    removing logging

--- backend/app_server/db/config.js
+++ backend/app_server/db/config.js
@@ -1,13 +1,10 @@
-console.log('Looking for .env file at:', require('path').resolve(__dirname, '../.env'));
 require('dotenv').config({ 
   path: require('path').resolve(__dirname, '../.env'),
   override: true
 });
-console.log('Loaded ENV Variables:', process.env);
 
 const mysql = require('mysql2/promise');
 
-// Create the connection pool using environment variables
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
@@ -18,12 +15,4 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-console.log('DB Config:', {
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
-// Export the pool directly
 module.exports = pool;
